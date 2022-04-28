d55ccbac0e1d0378f1397fa9caa5752621b1dd22
reppohc123 <33378890+reppohc123@users.noreply.github.com>
2022-04-28 17:35:40 +0200

    time

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 //Create a React app from scratch.
 import React from "react";
 import ReactDom from "react-dom";
+
+var today = new Date();
+var time = today.getHours() + ":" + today.getMinutes();
+
 //Show a single h1 that says "Good morning" if between midnight and 12PM.
 //or "Good Afternoon" if between 12PM and 6PM.
 ReactDom.render(
   <div>
     <h1>hello world</h1>
+    <p>{time}</p>
   </div>,
   document.getElementById("root")
 );
